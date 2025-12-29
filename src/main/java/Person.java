a2757b31b1f69c7b03389975fb88993128e7140c
Hung Hao Han <lequanghung.work@gmail.com>
2025-12-29 20:56:48 +0700

    Init project

---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public void gatLua() {
+        System.out.println("Dắt trâu đi cày");
+        System.out.println("Đi cấy");
+        System.out.println("Gặt lúa");
+    }
+}
