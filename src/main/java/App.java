efa98f1059c044b0719185a0cfbd019c38d2069d
Hung Hao Han <lequanghung.work@gmail.com>
2025-12-30 17:00:07 +0700

    Object and Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person person1 = new Person();
+        person1.setId(1);
+        person1.setFirstName("John");
+        person1.setLastName("Doe");
+
+        List<Person.Address>  addresses = new ArrayList<Person.Address>();
+
+        Person.Address address1 = new Person.Address();
+        address1.setStreet("17 đường số 7");
+        address1.setWard("phường Linh Trung");
+        address1.setDistrict("thành phố Thủ Đức");
+        address1.setProvince("thành phố Hồ Chí Minh");
+        addresses.add(address1);
+
+        person1.setAddresses(addresses);
+
+        System.out.println(person1);
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,11 +1,119 @@
+import java.util.List;
+
 public class Person {
     private long id;
     private String firstName;
     private String lastName;
+    private List<Address> addresses;
+
+    public Person() {
+    }
+
+    public Person(long id, String firstName, String lastName, List<Address> addresses) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.addresses = addresses;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public void gatLua() {
         System.out.println("Dắt trâu đi cày");
         System.out.println("Đi cấy");
         System.out.println("Gặt lúa");
+    }
+
+    static class Address {
+        private String street;
+        private String ward;
+        private String district;
+        private String province;
+
+        public Address() {
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getWard() {
+            return ward;
+        }
+
+        public void setWard(String ward) {
+            this.ward = ward;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        public void setDistrict(String district) {
+            this.district = district;
+        }
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "street='" + street + '\'' +
+                    ", ward='" + ward + '\'' +
+                    ", district='" + district + '\'' +
+                    ", province='" + province + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", addresses=" + addresses +
+                '}';
     }
 }
